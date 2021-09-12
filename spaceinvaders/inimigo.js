0599d63acdfc137c6606947da16043048b01df10
Brunimations <78793779+Brunimations@users.noreply.github.com>
2021-09-12 20:12:27 -0300

    fileuploads

---
+++ spaceinvaders/inimigo.js
@@ -0,0 +1,24 @@
+class Inimigo{
+    //variaveis
+    InimigoVivo;
+    GrupoDeInimigos;
+    TipoDeInimigo;
+    Velocidade;
+    xInimigo;
+    yInimigo;
+    TiroInimigo;
+    Pontos;
+   
+    Movimentar(){
+    /*é responsavel movimentar o conjunto de inimigos com base na
+    variavel de velocidade*/
+    }
+    Atirar(){
+    /*executa a o método serAtiradoPorPlayer da classe projetil ao
+    apertar espaço caso o tempo*/
+    }
+    Morrer(){
+    /*torna a opacidade do inimigo como 0 quando a variavel
+    inimigoVivo for falsa*/
+    }
+   }
---
+++ spaceinvaders/painel.js
@@ -0,0 +1,23 @@
+class Painel{
+
+    //variaveis
+
+    Nivel;
+    Player;
+    Inimigo;
+    Barricada;
+
+    Iniciar(){
+        /*ela chama as classes de player, barricada
+        e inimigo e posiciona elas no canvas, ao mesmo
+        tempo determina a variavel de nivel como 1*/
+    }
+
+    MudarDeNivel(){
+        /*verifica se todos os inimigos foram eliminados,
+        caso sim, o jogo acrescenta uma vida ao jogador,
+        aumenta a variavel de velocidade de inimigos e
+        os posiciona novamente*/
+    }
+
+}
